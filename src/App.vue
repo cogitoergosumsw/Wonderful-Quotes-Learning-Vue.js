3578991936c8457a63ece927791ba921b079d266
Seng Wee <sengwee_new@hotmail.com>
2019-03-02 22:49:14 +0800

    passing data with props and slots

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="container">
-
-    </div>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: function() {
-          return {
-            quotes: [
-              'Test Quote'
-            ],
-            maxQuote: 10
-          }
-        }
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Test Quote"],
+      maxQuote: 10
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="row">
+    <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+export default {
+  import Quote from './Quote.vue';
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
