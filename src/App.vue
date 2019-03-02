cde310e748457ff47326852a091836b68a99af9a
Seng Wee <sengwee_new@hotmail.com>
2019-03-02 22:40:17 +0800

    initializing app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function() {
+          return {
+            quotes: [
+              'Test Quote'
+            ],
+            maxQuote: 10
+          }
+        }
     }
 </script>
 
