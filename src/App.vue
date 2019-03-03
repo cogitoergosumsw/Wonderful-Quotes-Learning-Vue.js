b445be91bac85256b6e59a7a4377aa86ffb16b0f
Seng Wee <sengwee_new@hotmail.com>
2019-03-03 16:20:38 +0800

    adding info box

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,36 @@
   <div class="container">
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on quote to Delete it!
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
-import NewQuote from './components/NewQuote.vue';
+import NewQuote from "./components/NewQuote.vue";
 export default {
   data: function() {
     return {
       quotes: ["Test Quote"],
       maxQuote: 10
-    }
+    };
   },
   methods: {
     newQuote(quote) {
-       this.quotes.push(quote);
+      this.quotes.push(quote);
     }
   },
   components: {
     appQuoteGrid: QuoteGrid,
     appNewQuote: NewQuote
   }
-}
+};
 </script>
 
 <style></style>
